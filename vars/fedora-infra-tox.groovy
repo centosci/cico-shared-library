2cbd81421802ceaf91cac843d10e3a9499652a39
Brian Stinson <brian@bstinson.com>
2018-12-11 15:29:20 -0600

    get something working while we test out the constants

--- vars/fedora-infra-tox.groovy
+++ vars/fedora-infra-tox.groovy
@@ -1,16 +1,15 @@
 #!groovy
-import org.centos.ci.pipeline.lib.Constants
 
 def call(Map config) {
     def stages = [:]
     def fedora_containers = [
                     containerTemplate(name: 'jnlp',
-                                    image: "docker-registry.default.svc:5000/bstinson/agent-python3-tox:latest",
-                                    ttyEnabled: false,
-                                    args: '${computer.jnlpmac} ${computer.name}',
-                                    workingDir: "/workdir")
+                                      image: "docker-registry.default.svc:5000/bstinson/agent-python3-tox:latest",
+                                      ttyEnabled: false,
+                                      args: '${computer.jnlpmac} ${computer.name}',
+                                      workingDir: "/workdir")
     ]
-    //def active_fedoras = ["f28", "f29", "rawhide"]
+    def active_fedoras = ["f28", "f29", "rawhide"]
     def any_failed = false
 
     Constants.ACTIVE_FEDORAS.each { fedora ->
