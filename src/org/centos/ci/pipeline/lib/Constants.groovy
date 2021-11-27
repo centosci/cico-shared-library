c12ba2a69d47187d5b981efbd51231a13b510332
Nils Philippsen <nils@redhat.com>
2021-11-27 14:40:41 +0100

    Update Fedora and EL releases
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- src/org/centos/ci/pipeline/lib/Constants.groovy
+++ src/org/centos/ci/pipeline/lib/Constants.groovy
@@ -1,6 +1,6 @@
 package org.centos.ci.pipeline.lib
 
 class Constants {
-    public static final List<String> ACTIVE_FEDORAS = Collections.unmodifiableList(Arrays.asList("f30", "f31","f32", "latest"));
-    public static final List<String> ACTIVE_ELS = Collections.unmodifiableList(Arrays.asList("el6","el7"));
+    public static final List<String> ACTIVE_FEDORAS = Collections.unmodifiableList(Arrays.asList("f30", "f31","f32", "f33", "f34", "f35", "latest"));
+    public static final List<String> ACTIVE_ELS = Collections.unmodifiableList(Arrays.asList("el6","el7", "el8"));
 }
